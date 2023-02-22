f258067cc1a0b0afc27b0a04d72faaa21a80c1b5
BaoNguyenGi <126033274+BaoNguyenGi@users.noreply.github.com>
2023-02-22 02:18:21 +0000

    Entered psuedocode as a checklist

---
+++ main.py
@@ -0,0 +1,14 @@
+#                                    Program for clocking system
+
+#  Scan_Card
+#      Get code from card
+#      search database for card code
+#      Get the name of the card holder
+
+#  Get_Time
+#      Get data and time of scan
+
+#  Mark_Att
+#      Enter code and time of scan
+
+#  Display_name and time of att
